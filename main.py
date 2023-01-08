d005397990eb3ab547208ff86667bb71756a2809
Lukas Mandik <>
2023-01-08 19:51:19 +0100

    [build app] parsing json data

--- main.py
+++ main.py
@@ -1,16 +1,30 @@
-# This is a sample Python script.
+import json
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+# load and parsing json data  -->  name, cpu and memory usage, created_at, status and IP address
 
+with open('sample-data.json') as json_file:
+    data = json.load(json_file)
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+    for container in data:
+        print("name:", container["name"])
 
+        try:
+            print("usage cpu:", container["state"]["cpu"]["usage"])
+        except Exception as e:
+            print(f"usage cpu: None data {e}")
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('parsing json data')
+        try:
+            print("usage memory:", container["state"]["memory"]["usage"])
+        except Exception as e:
+            print(f"usage memory: None data {e}")
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+        print("created_at:", container["created_at"])
+
+        print("status:", container["status"])
+
+        try:
+            print("ip address:", container["state"]["network"]["eth0"]["addresses"][0]["address"])
+        except Exception as e:
+            print(f"ip address: None data {e}")
+
+        print("\n")
