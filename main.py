63ffcf611a54e6cefe0d1dc3074891733478297b
Lukas Mandik <>
2023-01-10 12:04:29 +0100

    [complete script] correction in the code, adding function deep_get for data parsing instead of using Exception handling.

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ import json
 from datetime import datetime
 import pytz
 import psycopg2
+from functools import reduce
 from db_info import db_host, db_name, db_port, db_username, db_password
+
 # load and parsing json data  -->  name, cpu and memory usage, created_at, status and IP address
 
 with open('sample-data.json') as json_file:
@@ -10,52 +12,33 @@ with open('sample-data.json') as json_file:
 
 data_for_db_sub = []
 data_for_db = []
-unavailable_data = "unavailable data"
 dt_str = []
 format_time = "%Y-%m-%dT%H:%M:%S%f%z"
 
+
+def deep_get(dictionary, keys, default="unavailable data"):
+    return reduce(lambda d, key: d.get(key, default) if isinstance(d, dict) else default, keys.split("."), dictionary)
+
+
 for container in data:
-    print("name:", container["name"])
-    data_for_db_sub.append(container["name"])
-
-    try:
-        print("usage cpu:", container["state"]["cpu"]["usage"])
-        data_for_db_sub.append(container["state"]["cpu"]["usage"])
-    except Exception as e:
-        print(f"usage cpu: None data {e}")
-        data_for_db_sub.append(unavailable_data)
-
-    try:
-        print("usage memory:", container["state"]["memory"]["usage"])
-        data_for_db_sub.append(container["state"]["memory"]["usage"])
-    except Exception as e:
-        print(f"usage memory: None data {e}")
-        data_for_db_sub.append(unavailable_data)
-
-    print("created_at:", container["created_at"])
+    data_for_db_sub.append(deep_get(container, "name"))
+    data_for_db_sub.append(deep_get(container, "state.cpu.usage"))
+    data_for_db_sub.append(deep_get(container, "state.memory.usage"))
+    print("created at get:", deep_get(container, "created_at"))
     # Convert local datetime to UTC time-zone datetime
-    dt_str.append(container["created_at"])
+    dt_str.append(deep_get(container, "created_at"))
     local_dt = datetime.strptime(dt_str[0], format_time)
     dt_utc = local_dt.astimezone(pytz.UTC)
     dt_utc_str = dt_utc.strftime(format_time)
     data_for_db_sub.append(dt_utc_str)
     dt_str.clear()
+    data_for_db_sub.append(deep_get(container, "status"))
 
-    print("status:", container["status"])
-    data_for_db_sub.append(container["status"])
-
-    try:
-        print("ip address:", container["state"]["network"]["eth0"]["addresses"][0]["address"])
-        data_for_db_sub.append(container["state"]["network"]["eth0"]["addresses"][0]["address"])
-    except Exception as e:
-        print(f"ip address: None data {e}")
-        data_for_db_sub.append(unavailable_data)
+    addresses_get = (deep_get(container, "state.network.eth0.addresses"))
+    data_for_db_sub.append(deep_get(addresses_get[0], "address"))
 
     data_for_db.append(tuple(data_for_db_sub))
     data_for_db_sub.clear()
-    print("\n")
-
-print(data_for_db)
 
 # import parsing data to database
 
@@ -84,14 +67,14 @@ cursor.execute("""
 
 query = """
     INSERT INTO parsing_data(
-    name, 
+    name,
     cpu_usage,
-    memory_usage, 
+    memory_usage,
     created_at,
     status,
     ip_address
-    
-    ) 
+
+    )
     VALUES (%s, %s, %s, %s, %s, %s)
 """
 
